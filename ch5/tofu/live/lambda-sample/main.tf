b3a89d278d1e848f388b4c9788ea359700e6c78d
Konrad Kerr <konradkerr@mail.weber.edu>
2026-02-18 01:09:30 -0700

    source revamp

--- ch5/tofu/live/lambda-sample/main.tf
+++ ch5/tofu/live/lambda-sample/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "function" {
-  source  = "github.com/KKerr25/devops-book/ch5/live/tofu/lambda-sample"
+  source  = "KKerr25/devops-book//ch5/live/tofu/lambda-sample"
   version = "1.0.0"
 
   name = var.name
