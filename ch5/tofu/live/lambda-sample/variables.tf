fe4291dad4d9a43e5661fc89886387e7f5ad8835
Konrad Kerr <konradkerr@mail.weber.edu>
2026-02-18 00:24:54 -0700

    add version

--- ch5/tofu/live/lambda-sample/variables.tf
+++ ch5/tofu/live/lambda-sample/variables.tf
@@ -3,3 +3,32 @@ variable "name" {
   type        = string
   default     = "lambda-sample"
 }
+
+variable "src_dir" {
+  type = string
+}
+
+variable "runtime" {
+  type = string
+}
+
+variable "handler" {
+  type = string
+}
+
+variable "memory_size" {
+  type = number
+}
+
+variable "timeout" {
+  type = number
+}
+
+variable "environment_variables" {
+  type = map(string)
+}
+
+variable "create_url" {
+  type    = bool
+  default = false
+}
